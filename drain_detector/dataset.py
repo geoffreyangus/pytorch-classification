8a2da1c3684344498282c68a466104ec057461cf
Geoffrey Angus <gdlangus@gmail.com>
2019-11-18 15:30:32 -0800

    moved stuff around, finished drain detection experiments

--- drain_detector/dataset.py
+++ drain_detector/dataset.py
@@ -7,17 +7,20 @@ import pandas as pd
 import torch
 from emmental.data import EmmentalDataset
 from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import transforms as custom_transforms
 from util import compose
 
 
 class DrainDetectionDataset(EmmentalDataset):
 
-    def __init__(self, split_dir, split_str, images_dir, transforms=None):
+    def __init__(self, split_dir, split_str, images_dir, transforms=None, cxr_only=False):
         """
         """
         split_path = osp.join(split_dir, 'split', f'{split_str}.csv')
         self.split_df = pd.read_csv(split_path, index_col=0)
         self.images_dir = images_dir
+        self.cxr_only = cxr_only
 
         if transforms:
             self.transforms = {
@@ -40,18 +43,25 @@ class DrainDetectionDataset(EmmentalDataset):
         """
         """
         image_id = self.X_dict['image_ids'][idx]
-        x1 = Image.open(osp.join(self.images_dir, f'{image_id[:-4]}_real_A2.png'))
-        if 'x1' in self.transforms:
-            x1 = self.transforms['x1'](x1)
-#         x2 = Image.open(osp.join(self.images_dir, f'{image_id[:-4]}_fake_B2.png'))
-#         if 'x2' in self.transforms:
-#             x2 = self.transforms['x2'](x2)
-        
-#         x = torch.cat((x1, x2), axis=0)
-        x = x1
-        if 'x' in self.transforms:
+        x1 = Image.open(osp.join(self.images_dir, f'{image_id[:-4]}_real_A2.png'))            
+        if self.cxr_only:
+            if 'x1' in self.transforms:
+                x1 = self.transforms['x1'](x1)
+            x = x1
+        else:
+            x1 = transforms.Grayscale(num_output_channels=1)(x1)
+            x1 = custom_transforms.Unsqueeze(axis=-1)(x1)
+            if 'x1' in self.transforms:
+                x1 = self.transforms['x1'](x1)
+            x2 = Image.open(osp.join(self.images_dir, f'{image_id[:-4]}_fake_B2.png'))
+            x2 = custom_transforms.ChooseChannels(channels=[1, 2])(x2)
+            if 'x2' in self.transforms:
+                x2 = self.transforms['x2'](x2)
+            x = torch.cat((x1, x2), axis=0)
+            
+        if 'joint' in self.transforms:
             x = self.transforms['joint'](x)
-        
+            
         x_dict = {k: v[idx] for k, v in self.X_dict.items() if k != 'image'}
         x_dict['image'] = x
         y_dict = {k: v[idx] for k, v in self.Y_dict.items()}        
---
+++ drain_detector/experiments/drain_detection/cxr_only/pretrain_imagenet/_sources/util_2774e082f3012ada65d055e1f8fe5608.py
@@ -0,0 +1,45 @@
+import os
+import os.path as osp
+
+import transforms as custom_transforms
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
+
+def compose(fn_list):
+    """
+    Compiles a list of functions described as {class_name: str, args: str}.
+    """
+    transforms_list = []
+    for f in fn_list:
+        class_name = f['class_name']
+        args = f['args']
+        if hasattr(transforms, class_name):
+            transforms_list.append(getattr(transforms, class_name)(**args))
+        else:
+            transforms_list.append(getattr(custom_transforms, class_name)(**args))
+    return transforms.Compose(transforms_list)
+
+
+def ce_loss(task_name, immediate_output, Y, active):
+    """
+    CrossEntropyLoss function to be used with Emmental module.
+    """
+    return F.cross_entropy(
+        immediate_output[f"decoder_module_{task_name}"][0], Y.view(-1)
+    )
+
+
+def output(task_name, immediate_output):
+    """
+    Softmax function to be used with Emmental module.
+    """
+    return F.softmax(immediate_output[f"decoder_module_{task_name}"][0], dim=1)
+
+
+def require_dir(dir_str):
+    """
+    """
+    if not(osp.exists(dir_str)):
+        require_dir(osp.dirname(dir_str))
+        os.mkdir(dir_str)
